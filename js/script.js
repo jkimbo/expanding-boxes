e3bac7472558bc937fdd2a473e807e5b864b8720
Jonathan Kim <jkimbo@gmail.com>
2011-11-12 01:19:57 +0000

    Got rid of loads of code :)

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var percentplus = percent/100 + 1;
     var percentminus = 1 - percent/100;
     var cont = $('#main');
-    var duration = 1000;
+    var duration = { duration: 1000, queue: false };
 
     $('.box').each(function(index) {
         this.origleft = parseInt($(this).css('left')); // store the original left value to element
@@ -29,16 +29,16 @@ $(document).ready(function() {
                 width: this.origwidth*percentplus,
                 height: this.origheight*percentplus,
                 left: this.origleft-(diff/2)
-            }, { duration: duration, queue: false });
+            }, duration);
             box.prev().animate({
                 width: prev.origwidth - (diff/2),
                 height: prev.origheight 
-            }, { duration: duration, queue: false });
+            }, duration);
             box.next().animate({
                 width: next.origwidth - (diff/2),
                 height: next.origheight,
                 left: next.origleft+(diff/2)
-            }, { duration: duration, queue: false });
+            }, duration);
         } else 
 
         /* Right box */     
@@ -47,11 +47,11 @@ $(document).ready(function() {
                 width: this.origwidth*percentplus,
                 height: this.origheight*percentplus,
                 left: this.origleft-(diff)
-            }, { duration: duration, queue: false });
+            }, duration);
             box.prev().animate({
                 width: prev.origwidth - (diff),
                 height: prev.origheight 
-            }, { duration: duration, queue: false });
+            }, duration);
         } else 
 
         /* Left box */     
@@ -59,12 +59,12 @@ $(document).ready(function() {
             box.animate({
                 width: this.origwidth*percentplus,
                 height: this.origheight*percentplus
-            }, { duration: duration, queue: false });
+            }, duration);
             box.next().animate({
                 width: next.origwidth - (diff),
                 height: next.origheight,
                 left: next.origleft+diff
-            }, { duration: duration, queue: false });
+            }, duration);
         }
     }, function() {
         var box = $(this);
@@ -72,42 +72,23 @@ $(document).ready(function() {
         var prev = box.prev().get(0);
         var next = box.next().get(0);
 
-        if(box.prev().length && box.next().length) { // center box
-            box.animate({
-                width: this.origwidth,
-                height: this.origheight,
-                left: this.origleft 
-            }, { duration: duration, queue: false });
+        box.animate({
+            width: this.origwidth,
+            height: this.origheight,
+            left: this.origleft 
+        }, duration);
+        if(prev) {
             box.prev().animate({
                 width: prev.origwidth,
                 height: prev.origheight,
-            }, { duration: duration, queue: false });
+            }, duration);
+        }
+        if(next) {
             box.next().animate({
                 width: next.origwidth,
                 height: next.origheight,
                 left: next.origleft
-            }, { duration: duration, queue: false });
-        } else if(box.prev().length) { // right box
-            box.animate({
-                width: this.origwidth,
-                height: this.origheight,
-                left: this.origleft
-            }, { duration: duration, queue: false });
-            box.prev().animate({
-                width: prev.origwidth,
-                height: prev.origheight,
-            }, { duration: duration, queue: false });
-        } else { // left box
-            box.animate({
-                width: this.origwidth,
-                height: this.origheight,
-                left: this.origleft
-            }, { duration: duration, queue: false });
-            box.next().animate({
-                width: next.origwidth,
-                height: next.origheight,
-                left: next.origleft
-            }, { duration: duration, queue: false });
+            }, duration);
         }
     });
 });
